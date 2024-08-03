9f2b1c4bf7912a990378495603413614698b5569
RebecaM <134107221+Rsenju@users.noreply.github.com>
2024-08-03 20:09:18 -0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h1 v-html="this.question">   
+    </h1>
+    <input type="radio" name="options" value="True">
+    <label>True</label><br>
+
+    <input type="radio" name="options" value="False">
+    <label>False</label><br>
+
+    <button class="send" type="button">Enviar</button>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name:"App",
+
+  data() {
+    return{
+      question: undefined,
+      incorrect_answers: undefined,
+      correct_answer: undefined,
+    }
+
+  },
+
+  created() {
+    this.axios
+    .get('https://opentdb.com/api.php?amount=1&category=26')
+    .then((response) => {
+      this.question = response.data.results[0].question;
+      this.incorrect_answers = response.data.results[0].incorrect_answers;
+      this.correct_answer = response.data.results[0].correct_answer;
+  })
+  }
+
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type=radio] {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
+
+}
+</style>
